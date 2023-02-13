b061c5e98bb309a007967245a60b5ae713cf8582
talexey78 <talexey78@gmail.com>
2023-02-13 03:00:30 +0300

    аааа

---
+++ py/module1.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      user
+#
+# Created:     12.02.2023
+# Copyright:   (c) user 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+
+    print("hello")
+    name=input("input name")
+    print("go",name )
+    pass
+
+if __name__ == '__main__':
+    main()
